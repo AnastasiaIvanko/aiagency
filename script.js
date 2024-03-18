3e3325e899028c7a398c18d283b6e2def544473e
AnastasiaIvanko <140964142+AnastasiaIvanko@users.noreply.github.com>
2024-03-18 14:33:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,80 @@ function openModal(modalId) {
     modal.style.top = scrollY + 50 + 'px'; // 50px - начальное смещение от верха, можно изменить по вашему усмотрению
   };
   
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    lightGallery(document.getElementById('gallery'), {
+        plugins: [lgThumbnail, lgFullscreen],
+        speed: 500
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    lightGallery(document.getElementById('horizontal-images'), {
+        selector: 'a',
+        mode: 'lg-fade',
+        download: false,
+        counter: false,
+        mousewheel: false,
+        swipeThreshold: 50
+    });
+});
+
+
+const menuIcon = document.querySelector('.menu-icon');
+const navList = document.querySelector('.nav-list');
+
+menuIcon.addEventListener('click', () => {
+    navList.classList.toggle('active'); // Добавляем/удаляем класс "active"
+});
+
+
+document.querySelectorAll('.service h3').forEach(function(item) {
+    item.addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+});
+
+
+
+
+
+
+
+// Функция для открытия модального окна
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+  }
+  
+  // Функция для закрытия модального окна
+  function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+  }
+ 
+  
+
+  window.onscroll = function() {
+    var modal = document.getElementById('yourModalId'); // Замените 'yourModalId' на ID вашего модального окна
+    var scrollY = window.scrollY || document.documentElement.scrollTop;
+    modal.style.top = scrollY + 50 + 'px'; // 50px - начальное смещение от верха, можно изменить по вашему усмотрению
+  };
+  
+
+  // Функция для открытия модального окна
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+
+    modal.style.display = "block";
+    modal.style.top = scrollY + "px"; // Устанавливаем top в текущее положение прокрутки
+}
+
+// Функция для закрытия модального окна
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
